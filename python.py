90f06ee3737e226593bf6191db97126024e55b96
madscientist-314 <tomkirby314@gmail.com>
2022-10-07 20:22:33 +0100

    07.10.22 - Daily - Base controls work!

--- python.py
+++ python.py
@@ -1,35 +1,51 @@
-from pickle import TRUE
 import sys
-from tkinter import ON
+import os
+from turtle import speed
 import pip
-import pygame
+import random as rand
+import pygame as pg
+from pickle import TRUE
 from pygame import QUIT
+from tkinter import ON
 
-pygame.init()
-surface = pygame.display.set_mode((1080, 1080))
-clock = pygame.time.Clock()
-surfrect = surface.get_rect()
-rect = pygame.Rect((0, 0), (128, 128))
-rect.center = (surfrect.w / 2, surfrect.h / 2)
-touched = False
+#init pygame
+pg.init()
+
+#set h+w of screen
+size = [1280, 720]
+screen = pg.display.set_mode(size)
+
+#window name
+title = "Non-Euclidian Maze"
+pg.display.set_caption(title)
+
+#loop untill close button pressed
+clock = pg.time.Clock()
 g_ON = True
-while g_ON == True:
-  for ev in pygame.event.get():
-    if ev.type == QUIT:
-      pygame.quit()
-    elif ev.type == pygame.MOUSEBUTTONDOWN:
-      if rect.collidepoint(ev.pos):
-        touched = True
-        pygame.mouse.get_rel()
-    elif ev.type == pygame.MOUSEBUTTONUP:
-      touched = False
-  clock.tick(60)
-  surface.fill((0, 0, 0))
-  if touched:
-    rect.move_ip(pygame.mouse.get_rel())
-    rect.clamp_ip(surfrect)
-  surface.fill((255, 255, 255), rect)
-  pygame.display.flip()
 
-  while touched:
-    pg.mouse.get_rel()
+#player rect size and start pos
+rect = pg.Rect(0,0,32,32)
+rect.center = (size[0] / 2, size[1] / 2)
+
+playerspeed = 32
+
+while g_ON:
+    clock.tick(60)
+    screen.fill((0, 0, 0))
+    
+    for ev in pg.event.get():
+        if ev.type == QUIT:
+            pg.quit()
+        elif ev.type == pg.KEYDOWN:
+            if ev.key == pg.K_LEFT or ev.key == pg.K_a:
+                rect = pg.Rect.move(rect, -playerspeed, 0)
+            if ev.key == pg.K_RIGHT or ev.key == pg.K_d:
+                rect = pg.Rect.move(rect, playerspeed, 0)
+            if ev.key == pg.K_UP or ev.key == pg.K_w:
+              rect = pg.Rect.move(rect, 0, -playerspeed)
+            if ev.key == pg.K_DOWN or ev.key == pg.K_s:
+              rect = pg.Rect.move(rect, 0, playerspeed)
+    
+    screen.fill((255, 255, 255), rect)
+    pg.display.flip()
+pg.quit()
